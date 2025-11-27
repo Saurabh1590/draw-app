e362fa393aa929e5cabac0f508ba22326b737e00
Saurabh Sahani <saurabhsahani2710@gmail.com>
2025-11-28 01:20:01 +0530

    feat(auth): implement database persistence for user signup

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,59 +1,76 @@
 import express from "express";
 import { JWT_SECRET_KEY } from "@repo/backend-common/config";
 import { middleware } from "./auth/middleware";
-import {CreateUserSchema, SignInSchema, CreateRoomSchema} from "@repo/common/types";
-
+import {
+  CreateUserSchema,
+  SignInSchema,
+  CreateRoomSchema,
+} from "@repo/common/types";
+import { prismaClient } from "@repo/database/client";
 const jwt = require("jsonwebtoken");
-
-
 
 const app = express();
 
-app.post("/signup", (req, res) => {
-    const data = CreateUserSchema.safeParse(req.body);
+app.post("/signup", async (req, res) => {
+  const parsedData = CreateUserSchema.safeParse(req.body);
 
-    if(!data.success) {
-        return res.json({
-            message: "Incorrect Input"
-        })
-    }
+  if (!parsedData.success) {
+    return res.json({
+      message: "Incorrect Input",
+    });
+  }
 
-    res.json({
-        userId: "123"
+  try{
+    const User = await prismaClient.user.create({
+    data: {
+      email: parsedData.data?.email,
+      password: parsedData.data?.password,
+      name: parsedData.data?.name,
+    },
+  });
+  res.json({
+    userId: "123",
+  });
+  } catch (error) {
+    res.status(411).json({
+        message: "User already exists with the username"
     })
-
-})
+  }
+  
+});
 
 app.post("/signin", (req, res) => {
+  const data = SignInSchema.safeParse(req.body);
 
-    const data = SignInSchema.safeParse(req.body);
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect Input",
+    });
+  }
+  const userId = 1;
+  const token = jwt.sign(
+    {
+      userId,
+    },
+    JWT_SECRET_KEY
+  );
 
-    if(!data.success) {
-        return res.json({
-            message: "Incorrect Input"
-        })
-    }
-    const userId = 1;
-    const token = jwt.sign({
-        userId
-    }, JWT_SECRET_KEY)
-
-    res.json({
-        token
-    })
-})
+  res.json({
+    token,
+  });
+});
 
 app.post("/room/:id", middleware, (req, res) => {
-    const data = CreateRoomSchema.safeParse(req.body);
+  const data = CreateRoomSchema.safeParse(req.body);
 
-    if(!data.success) {
-        return res.json({
-            message: "Incorrect Input"
-        })
-    }
-    res.json({
-        roomId: 123
-    })
-})
+  if (!data.success) {
+    return res.json({
+      message: "Incorrect Input",
+    });
+  }
+  res.json({
+    roomId: 123,
+  });
+});
 
 app.listen(3001);
